61c9fe4119e4301e97cf11090d31c6d016b83bb2
Ishav <106452155+0007ishav@users.noreply.github.com>
2024-01-14 15:25:54 +0000

    functions

--- Basics03/functions01.js
+++ Basics03/functions01.js
@@ -25,4 +25,46 @@ function loginUserMessage(username = "Sam") {    // Default value
 }
 
 // console.log(loginUserMessage("Ishav"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+// function calculateCartPrice(num1) {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(2));
+
+
+// function calculateCartPrice(...num1) {  // Here, Three dots are rest operator.
+//     return num1
+// }
+// console.log(calculateCartPrice(2,4,5));
+
+
+function calculateCartPrice(val1, val2, ...num1) {  // Here, Three dots are rest operator.
+    return num1
+}
+// console.log(calculateCartPrice(2,4,5,7));
+
+const user = {
+    username: "Ishav",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
