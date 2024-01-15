2516e5092336f3abf3b21c63cc17ae5d4e99b97b
Ishav <106452155+0007ishav@users.noreply.github.com>
2024-01-15 11:55:56 +0000

    for

---
+++ Iterations/for.js
@@ -0,0 +1,13 @@
+// For
+
+for (let i = 0; i <= 7; i++) {
+    const element = i;
+    
+    if (element == 7) {
+        console.log("7 is best number");
+    }
+    console.log(element);
+}
+
+// console.log(element);
+
