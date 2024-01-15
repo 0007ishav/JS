9265d9adfff489c9f0a2b5db56768d00c8b6e96b
Ishav <106452155+0007ishav@users.noreply.github.com>
2024-01-15 10:58:44 +0000

    switch

---
+++ control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+switch (month) {
+    case 1:
+        console.log("jan"); 
+        break;
+   
+    case "feb" :
+        console.log("Feb"); 
+        break;
+   
+    case 3:
+        console.log("March"); 
+        break;
+   
+    default:
+        console.log("Add a month number");
+        break;
+}
